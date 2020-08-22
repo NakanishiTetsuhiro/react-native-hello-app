54bdc4af10973175781875534d58a5ae09f969fb
TETSU <kaoru6strings@gmail.com>
2020-08-22 12:20:15 +0900

    これをやった https://swallow-incubate.com/archives/blog/20191220

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import Page1DetailScreen from './screens/Page1DetailScreen';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import Page1Screen from './screens/Page1Screen';
+import Page2Screen from "./screens/Page2Screen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const MainStack = createStackNavigator(
+  {
+    Page1: Page1Screen,
+    Page1Detail: Page1DetailScreen,
+  }
+)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const TabNavigator = createBottomTabNavigator(
+  {
+    Page1: {
+      screen: MainStack
+    },
+    Page2: Page2Screen,
   },
-});
+  {
+    initialRouteName: 'Page1'
+  }
+)
+
+const AppContainer = createAppContainer(TabNavigator)
+
+export default class App extends Component {
+  render() {
+    return (
+      <AppContainer />
+    )
+  }
+}
